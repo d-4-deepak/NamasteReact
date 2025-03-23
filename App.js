dfe9ab5b25de88a6fedf6df634764111591cc469
Deepak <dg934257@gmail.com>
2025-03-23 10:31:52 +0530

    epispde 3

--- App.js
+++ App.js
@@ -2,7 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
-const parent = React.createElement("div",{id:"parent"},"parent Element");
+// const heading = React.createElement("h1",{id:"h1"},"Namasate React");
+
+const jsxHeading = <h1 id="h1">Namaste React JS</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+const reactEle = <h2>The react Element is  stored in JS variable in the end of the day</h2>
+
+const Title = ()=>(
+    <h1 className="head" tabIndex="5">
+        Namaste React Using JSx
+    </h1>
+)
+
+const HeadingComponent = ()=>(
+    <div>
+        <Title/>
+        {reactEle}
+        <h1>This is Functional Component</h1>
+    </div>
+)
+
+root.render(<HeadingComponent/>);
